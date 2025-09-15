b514ac0fae78ff909fff9e212af4c458ab02e944
kidxash <ASHTONSINGLETON125@GMAIL.COM>
2025-09-15 16:07:19 -0400

    added JWT packages with an update to the  user models

--- User_Models.js
+++ User_Models.js
@@ -3,7 +3,12 @@ import { mongoose } from "mongoose";
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    verifyOtp: { type: String, required: false },
+    isAccountVerified: { type: Boolean, default: false },
+    resetOTP: { type: String, required: false },
+    resetOTPExpireAt: {type: Number, default: 0},
+
 });
 
 const User = mongoose.model("User", userSchema);
